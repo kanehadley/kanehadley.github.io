cfb4a2dea84d5d1f92227c30cde38971811c4099
Kane Hadley <Kane Hadley>
2015-08-24 21:05:02 -0400

    Scatter plot has sliders.

--- js/education.js
+++ js/education.js
@@ -50,11 +50,46 @@ function main () {
 }
 
 function education (data) {
+
+
     var div = d3.select('#education-bar-chart');
 
     var chart = new BarChart(div.node());
 
     chart.render(generateEducationData(data));
+
+    var div2 = d3.select('#education-scatter-plot');
+
+    var scatter = new ScatterPlot(div2.node());
+
+    //var meanSlider = d3.select('#mean');
+    //var varianceSlider = d3.select('#variance');
+    var meanSlider = $('#mean');
+    var varianceSlider = $('#variance');
+
+    //meanSlider
+    $('#mean')
+        .on('change mousemove', function () {
+            scatter.render(generateEducationScatterData(data, parseFloat(meanSlider.val()), parseFloat(varianceSlider.val())));
+        });
+
+    varianceSlider
+        .on('change mousemove', function () {
+            scatter.render(generateEducationScatterData(data, parseFloat(meanSlider.val()), parseFloat(varianceSlider.val())));
+        });
+
+    scatter.render(generateEducationScatterData(data, 0.5, 0.5));
+}
+
+function generateEducationScatterData (data, mean, variance) {
+
+    var f = d3.random.normal(mean, variance);
+    return d3.range(100).map(function (d) {
+        return {
+            'x': f(),
+            'y': f()
+        };
+    });
 }
 
 function generateEducationData (data) {
@@ -118,8 +153,8 @@ function BarChart (div) {
         _xAxis.call(xAxis);
         _yAxis.call(yAxis);
 
-        var bars = _viewer.selectAll('rect').
-            data(data);
+        var bars = _viewer.selectAll('rect')
+            .data(data);
 
         bars.exit().remove();
 
@@ -156,4 +191,100 @@ function BarChart (div) {
     this.render = render;
 
     return this;
+}
+
+function ScatterPlot (div) {
+    var width = 1000;
+    var height = 500;
+
+    var _svg = d3.select(div).append('svg')
+        .attr({
+            'width': width,
+            'height': height
+        });
+
+    var _yAxis = _svg.append('g')
+        .attr({
+            'transform': 'translate(' + (width * 0.2) + ',0)',
+            'class': 'axis'
+        });
+
+    var _xAxis = _svg.append('g')
+        .attr({
+            'transform': 'translate(' + (width * 0.2) + ' ,' + (height * 0.8) + ')',
+            'class': 'axis'
+        });
+    var _viewer = _svg.append('g')
+        .attr('transform', 'translate(' + (width * 0.2) + ',0)');
+    var _border = _svg.append('rect')
+        .attr({
+            'transform': 'translate(' + (width * 0.2) + ',0)',
+            'width': width * 0.8,
+            'height': height * 0.8
+        })
+        .style({
+            'fill': 'none',
+            'stroke': 'black'
+        });
+
+    function render (data) {
+        var y = d3.scale.linear()
+            .range([height * 0.8, 0])
+            .domain([minYValue(data), maxYValue(data)]);
+
+        var x = d3.scale.linear()
+            .range([0, width * 0.8])
+            .domain([minXValue(data), maxXValue(data)]);
+
+        var yAxis = d3.svg.axis()
+            .scale(y)
+            .orient('left');
+
+        var xAxis = d3.svg.axis()
+            .scale(x)
+            .orient('bottom');
+
+        _yAxis.call(yAxis);
+        _xAxis.call(xAxis);
+
+        var points = _viewer.selectAll('circle')
+            .data(data);
+
+        points.exit().remove();
+
+        points.enter().append('circle');
+
+        points.attr({
+            'class': 'point',
+            'cx': function (d) { return x(d.x); },
+            'cy': function (d) { return y(d.y); },
+            'r': 3
+        });
+    }
+
+    function maxXValue (data) {
+        return d3.max(data.map(function (d) {
+            return d.x;
+        }));
+    }
+
+    function minXValue (data) {
+        return d3.min(data.map(function (d) {
+            return d.x;
+        }));
+    }
+
+    function maxYValue (data) {
+        return d3.max(data.map(function (d) {
+            return d.y;
+        }));
+    }
+
+    function minYValue (data) {
+        return d3.min(data.map(function (d) {
+            return d.y;
+        }));
+    }
+
+    this.render = render;
 }
