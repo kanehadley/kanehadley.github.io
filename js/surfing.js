73fed1519e134714b01414f0ac52d4143ec3467a
Kane Hadley <Kane Hadley>
2015-12-25 02:37:41 -0500

    Beginning to add click events.

--- js/surfing.js
+++ js/surfing.js
@@ -1,16 +1,48 @@
-function setup () {
-    $('#adder').click(function (e) {
-        var text = $('#channelBox').val();
-        console.log('Adding Channels!');
-        var pre = "<iframe width=\"560\" height=\"315\" src=\"";
-        var post = "\" frameborder=\"0\" allowfullscreen></iframe>";
-        $('#viewer').append(pre + parseYoutubeUrl(text) + post);
-    });
-}
+function surfing () {
+    'use strict';
 
-function parseYoutubeUrl (url) {
-    var parsing = url.trim().split('watch?v=');
-    var http = parsing[0];
-    var video = parsing[1];
-    return http + 'embed/' + video;
+    var channels = [];
+
+    function setup () {
+        $('#adder').click(function (e) {
+            var text = $('#channelBox').val();
+            console.log('Adding Channel ' + text + '!');
+            channels.push(youtubeUrlToObject(text));
+            $('#viewer').append(urlToIframe(parseYoutubeUrl(text)));
+            generateButtons();
+        });
+    }
+    
+    function parseYoutubeUrl (url) {
+        var parsing = url.trim().split('watch?v=');
+        var http = parsing[0];
+        var video = parsing[1];
+        return http + 'embed/' + video;
+    }
+
+    function urlToIframe(url) {
+        var pre = "<iframe width=\"560\" height=\"315\" src=\"",
+            post = "\" frameborder=\"0\" allowfullscreen></iframe>";
+        return pre + url + post;
+    }
+
+    function youtubeUrlToObject (url) {
+        return {
+            'url': url,
+            'frameUrl': parseYoutubeUrl(url)
+        };
+    }
+
+    function generateButtons () {
+        var buttons = d3.select('#channelButtons').selectAll('div')
+                        .data(channels);
+        buttons.exit().remove();
+        buttons.enter().append('div').attr({
+            'class': 'channelButton'
+        }).click(function (d) {
+
+        }).text(function (d) { return d.url; });
+    }
+
+    setup();
 }
